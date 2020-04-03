b63e05a715cf9f9e661c33cc4286fa5bd353b847
Kenneth Ofosu <kofosu89@gmail.com>
2020-04-03 16:16:39 -0400

    add styles

--- src/components/GlobalCard.js
+++ src/components/GlobalCard.js
@@ -4,28 +4,28 @@ import '../App.css'
 function GlobalCard(props) {
 	console.log(props.global);
 	return (
-		<div>
-			<div>
+		<div className='card-container'>
+			<div className='card'>
 				<h1>Global</h1>
-				<div>
+				<div className='scrollable'>
 					<table>
 						<thead>
 							<tr>
-								<th>Country</th>
-								<th>Total Cases</th>
-								<th>New Cases</th>
-								<th>Deaths</th>
-								<th>Recoveries</th>
+								<th className='headers'>Country</th>
+								<th className='headers'>Total Cases</th>
+								<th className='headers'>New Cases</th>
+								<th className='headers'>Deaths</th>
+								<th className='headers'>Recoveries</th>
 							</tr>
 						</thead>
 						<tbody>
 							{props.global.map(country => (
 								<tr>
-									<td>{country.Country}</td>
-									<td>{country.TotalConfirmed}</td>
-									<td>{country.NewConfirmed}</td>
-									<td>{country.TotalDeaths}</td>
-									<td>{country.TotalRecovered}</td>
+									<td className='name'>{country.Country}</td>
+									<td className='confirmed'>{country.TotalConfirmed}</td>
+									<td className='confirmed'>{country.NewConfirmed}</td>
+									<td className='deaths'>{country.TotalDeaths}</td>
+									<td className='recovered'>{country.TotalRecovered}</td>
 								</tr>
 							))}
 						</tbody>
