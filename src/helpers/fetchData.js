e40e115d659bdf271a0400cee07b038b63117bcc
Kenneth Ofosu <kofosu89@gmail.com>
2020-04-23 16:44:08 -0400

    edit getUsa axios request

--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const corsProxy = 'https://cors-anywhere.herokuapp.com/';
 
 export const getGlobal = axios.get(`https://api.covid19api.com/summary`)
-		
-export const getUsa = axios.get('https://corona.lmao.ninja/states')
-			
-export const getAusCan = axios.get(`${corsProxy}https://covid19api.xapix.io/v2/locations`)
 
- 
+export const getUsa = axios.get('https://corona.lmao.ninja/v2/states')
+
+export const getAusCan = axios.get(`${corsProxy}https://covid19api.xapix.io/v2/locations`)
